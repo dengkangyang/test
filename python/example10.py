d4defe88d913af7944bc5b1cd49f35ff0c611529
dengkangyang <dengkangyang@sina.com>
2017-03-27 09:31:04 +0800

    kstrstr

---
+++ python/example10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#coding=UTF-8
+
+import time
+
+print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+time.sleep(5)
+
+print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+
